6585cb3fc39dfc19258e80176c7b0eac54d82f8e
Andrew Kolos <andrewrkolos@gmail.com>
2023-11-06 09:33:17 -0800

    flavor_specific_assets: add android flavor support

--- flavor_specific_assets/lib/main.dart
+++ flavor_specific_assets/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+late final List<String> assets;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  assets = (await AssetManifest.loadFromAssetBundle(rootBundle)).listAssets();
   runApp(const MainApp());
 }
 
@@ -9,10 +13,38 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '--flavor',
+                      style:
+                          Theme.of(context).textTheme.headlineMedium!.copyWith(
+                                fontFamily: 'monaco',
+                                fontFamilyFallback: ['consolas', 'courier new'],
+                                backgroundColor: Color.fromRGBO(0, 0, 0, 0.1),
+                              ),
+                    ),
+                    TextSpan(
+                        text: ': $appFlavor',
+                        style: Theme.of(context).textTheme.headlineMedium)
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Assets',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              for (final asset in assets) Text('* $asset')
+            ],
+          ),
         ),
       ),
     );
