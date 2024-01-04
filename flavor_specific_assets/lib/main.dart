246f4f58a73ca187509c3497452e49753e5d2ef1
Andrew Kolos <andrewrkolos@gmail.com>
2024-01-03 16:03:40 -0800

    flavor_specific_assets: make app test-friendly

--- flavor_specific_assets/lib/main.dart
+++ flavor_specific_assets/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-late final List<String> assets;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  assets = (await AssetManifest.loadFromAssetBundle(rootBundle)).listAssets();
   runApp(const MainApp());
 }
 
@@ -16,37 +14,62 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: FutureBuilder(
+              future: AssetManifest.loadFromAssetBundle(rootBundle),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  final assets = snapshot.data!.listAssets();
+                  return MainWidget(assets: assets);
+                }
+              }),
+        ),
+      ),
+    );
+  }
+}
+
+class MainWidget extends StatelessWidget {
+  const MainWidget({
+    super.key,
+    required this.assets,
+  });
+
+  final List<String> assets;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RichText(
+          text: TextSpan(
             children: [
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: '--flavor',
-                      style:
-                          Theme.of(context).textTheme.headlineMedium!.copyWith(
-                                fontFamily: 'monaco',
-                                fontFamilyFallback: ['consolas', 'courier new'],
-                                backgroundColor: const Color.fromRGBO(0, 0, 0, 0.1),
-                              ),
+              TextSpan(
+                text: '--flavor',
+                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      fontFamily: 'monaco',
+                      fontFamilyFallback: ['consolas', 'courier new'],
+                      backgroundColor: const Color.fromRGBO(0, 0, 0, 0.1),
                     ),
-                    TextSpan(
-                        text: ': $appFlavor',
-                        style: Theme.of(context).textTheme.headlineMedium)
-                  ],
-                ),
               ),
-              const SizedBox(height: 20),
-              Text(
-                'Assets',
+              TextSpan(
+                text: ': $appFlavor',
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
-              for (final asset in assets) Text('* $asset')
             ],
           ),
         ),
-      ),
+        const SizedBox(height: 20),
+        Text(
+          'Assets',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        for (final asset in assets) Text('* $asset'),
+      ],
     );
   }
 }
